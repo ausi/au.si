8326dab3df3ebb15197d30213eea03a562b94f4b
Martin Auswöger <martin@auswoeger.com>
2015-09-06 18:58:08 +0200

    Use a local profile image and favicon

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var crypto = require('crypto');
 var sass = require('node-sass');
 var autoprefixer = require('autoprefixer-core');
 var postcss = require('postcss');
@@ -7,6 +8,8 @@ var cssnano = require('cssnano');
 var uncss = require('uncss');
 var marked = require('marked');
 var mustache = require('mustache');
+var request = require('request');
+var lwip = require('lwip');
 
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -82,6 +85,14 @@ pagesQueue.push({
 });
 
 buildPages();
+buildImages(function(images) {
+	if (!fs.existsSync(path.join(__dirname, 'dist', 'images'))) {
+		fs.mkdirSync(path.join(__dirname, 'dist', 'images'));
+	}
+	images.forEach(function(image) {
+		fs.writeFileSync(path.join(__dirname, 'dist', 'images', image.split('/').pop()), fs.readFileSync(image));
+	});
+});
 
 function buildPages() {
 	if (!pagesQueue.length) {
@@ -128,5 +139,57 @@ function stripTags(html) {
 function minifyHtml(html) {
 	return html.replace(/(<pre[^>]*>.*?<\/pre>)|(\n)\s+/gi, function(match, pre, lineBreak) {
 		return pre || lineBreak;
+	});
+}
+
+function buildImages(callback) {
+	request({
+		url: 'https://www.gravatar.com/avatar/ef0606c138d944e1a8089ff8eb1df71d?s=512',
+		encoding: null,
+	}, function (error, response, originalImage) {
+		if (error || response.statusCode !== 200) {
+			console.error(error);
+			console.error(response.statusCode);
+			callback();
+			return;
+		}
+		var imageHash = crypto.createHash('md5').update(originalImage).digest('hex');
+		var sizes = [64, 128];
+		var images = sizes.map(function(size) {
+			return path.join(__dirname, 'tmp', imageHash, 'profile-' + size + '.jpg');
+		});
+		if (fs.existsSync(path.join(__dirname, 'tmp', imageHash))) {
+			callback(images);
+			return;
+		}
+		if (!fs.existsSync(path.join(__dirname, 'tmp'))) {
+			fs.mkdirSync(path.join(__dirname, 'tmp'));
+		}
+		fs.mkdirSync(path.join(__dirname, 'tmp', imageHash));
+		lwip.open(originalImage, 'jpg', function(err, image){
+			if (err) {
+				console.error(err);
+				callback();
+				return;
+			}
+			var done = 0;
+			sizes.forEach(function(size) {
+				image.clone(function(err, clone) {
+					if (err) {
+						console.error(err);
+						return;
+					}
+					clone.batch()
+						.resize(size, size)
+						.writeFile(path.join(__dirname, 'tmp', imageHash, 'profile-' + size + '.jpg'), 'jpg', {quality: 85}, function(err) {
+							console.log('profile-' + size + '.jpg');
+							done++;
+							if (done === sizes.length) {
+								callback(images);
+							}
+						});
+				});
+			});
+		});
 	});
 }
